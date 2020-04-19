63ee06317f70fe2a60102625439424342aa9a4c2
ADMINISTRATOR\Administrator <lws912@163.com>
2020-04-19 12:59:33 +0800

    添加打卡页和分数页

--- control/func/pass.js
+++ control/func/pass.js
@@ -13,34 +13,62 @@ function initPassView(passIndex) {
     var topMedal = $('#passFinish .top-medalContainer .icon')
     var bottomItems = Array.from($('#passFinish .bottom-medalContainer .item'))
     topTitle.text("恭喜你完成第" + topTitles[passIndex] + "关，成功获得一枚勋章")
-    console.log(topMedal)
     topMedal.attr("src", './img/topMedal-' + (passIndex + 1) + '.png')
-    bottomItems.forEach((element,index) => {
+    bottomItems.forEach((element, index) => {
         var bottomImg = element.children[0]
         var bottomTitle = element.children[1]
         bottomTitle.style.color = "#333"
-        console.log(bottomImg)
         if (index === 0) {
             // 第一个
             // bottomImg.attr("src", './img/bottomMedal-1.png')
             bottomImg.src = './img/bottomMedal-1.png'
-        }else if (index === 1) {
+        } else if (index === 1) {
             // 第二个
             if (passIndex < 1) {
                 bottomImg.src = './img/disMedal-2.png'
                 bottomTitle.style.color = "#CCC"
-            }else {
+            } else {
                 bottomImg.src = './img/bottomMedal-2.png'
             }
-            
-        }else if (index === 2) {
+
+        } else if (index === 2) {
             // 第三个
             if (passIndex < 2) {
                 bottomImg.src = './img/disMedal-3.png'
                 bottomTitle.style.color = "#CCC"
-            }else {
+            } else {
                 bottomImg.src = './img/bottomMedal-3.png'
             }
         }
     });
+}
+
+// 设置分数页面
+function initScoreView(score, wrongItems) {
+    var scoreDom = Array.from($('.pointSection .scoreTitle'))[0]
+    scoreDom.innerHTML = score + '分'
+
+    var wrongContainer = $('.pointSection .wrongContainer')
+    wrongItems.forEach(element => {
+        var appendHtml = '<div class="wrongItem">' + element + '<img src="./img/wrongItem.png" alt=""></div>'
+        wrongContainer.append(appendHtml);
+    });
+}
+
+// 设置打卡页面数据
+function initClockView(name, content, school) {
+    var contentDoms = Array.from($('.clockSection .container p'));
+    console.log(contentDoms)
+    contentDoms.forEach((element, index) => {
+        if (index === 0) {
+            // 标题
+            element.innerHTML = name;
+        }
+        if (index === 1) {
+            element.innerHTML = content;
+        }
+        if (index === 2) {
+            element.innerHTML = school;
+        }
+    })
 }
